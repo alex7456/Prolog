9343d1ac373450a68edbd31c6826ca663184f220
alex7456 <aleksandrstrrjnyj@gmail.com>
2025-04-22 20:09:45 +0300

    refact

--- 9_5.pl
+++ 9_5.pl
@@ -1,13 +1,10 @@
-% Предикат чтения
 read_number(Number) :-
     write('Введите натуральное число: '),
     read(Number),
     integer(Number),
     Number > 0.
 
-% Предикаты логики (основные)
 
-% Главный предикат логики (минимальное количество аргументов)
 main_logic(Number) :-
     max_prime_divisor(Number, MaxPrime),
     product_of_digits(Number, Product),
@@ -15,20 +12,17 @@ main_logic(Number) :-
     gcd(MaxOddComposite, Product, GCD),
     print_results(Number, MaxPrime, GCD).
 
-% Нахождение максимального простого делителя
 max_prime_divisor(Number, MaxPrime) :-
     findall(D, (divider(Number, D), Divisors),
     include(is_prime, Divisors, PrimeDivisors),
     max_list(PrimeDivisors, MaxPrime).
 
-% Нахождение максимального нечетного непростого делителя
 max_odd_composite_divisor(Number, Div) :-
     findall(D, (divider(Number, D), D mod 2 =\= 0, \+ is_prime(D)), Divisors),
     (   Divisors = [] -> Div = 1 % если нет таких делителей
     ;   max_list(Divisors, Div)
     ).
 
-% Нахождение произведения цифр числа (рекурсия вверх)
 product_of_digits(Number, Product) :-
     product_of_digits(Number, 1, Product).
 
@@ -40,7 +34,6 @@ product_of_digits(Number, Acc, Product) :-
     NewNumber is Number // 10,
     product_of_digits(NewNumber, NewAcc, Product).
 
-% Предикат вывода
 print_results(Number, MaxPrime, GCD) :-
     nl,
     write('Для числа '), write(Number), write(':'), nl,
@@ -49,9 +42,7 @@ print_results(Number, MaxPrime, GCD) :-
     write('   и произведения цифр числа: '), write(GCD), nl,
     nl.
 
-% Вспомогательные предикаты (реализованные ранее)
 
-% Проверка на простоту числа
 is_prime(2) :- !.
 is_prime(3) :- !.
 is_prime(N) :-
@@ -64,19 +55,16 @@ has_factor(N, F) :-
     (N mod F =:= 0 -> true; 
      NewF is F + 2, has_factor(N, NewF)).
 
-% Нахождение всех делителей числа
 divider(N, D) :-
     between(1, N, D),
     N mod D =:= 0.
 
-% Нахождение НОД двух чисел
 gcd(A, 0, A) :- !.
 gcd(A, B, GCD) :-
     B > 0,
     R is A mod B,
     gcd(B, R, GCD).
 
-% Главный предикат
 start :-
     read_number(Number),
     main_logic(Number).
